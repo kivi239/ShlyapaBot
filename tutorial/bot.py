64673fa00c48e28c7a4bb74b0cdbe959ee0fd112
nosov-pvl <nosov-pvl@yandex.ru>
2016-02-28 18:31:41 +0300

    1: Added tutorial bot

---
+++ tutorial/bot.py
@@ -0,0 +1,40 @@
+import config
+import telebot
+
+bot = telebot.TeleBot(config.token)
+
+account=0
+
+def add_answer(num):
+    global account
+    account += num
+
+
+@bot.message_handler(commands = ['add'])
+def answerer(m):
+    num = int(m.text.split()[1]) if len(m.text.split()) > 1 else 0
+    add_answer(num)
+
+@bot.message_handler(commands = ['sub'])
+def answerer(m):
+    num = int(m.text.split()[1]) if len(m.text.split()) > 1 else 0
+    add_answer(-num)
+
+@bot.message_handler(commands = ['answer'])
+def answerer(m):
+    bot.send_message(m.chat.id, str(account))
+
+@bot.message_handler(commands = ['start'])
+def welcomer(m):
+    bot.send_message(m.chat.id, "Welcome!")
+
+@bot.message_handler(commands = ['help'])
+def welcomer(m):
+    bot.send_message(m.chat.id, "Help:\n /add N to add N\n /sub N to substract N \n /answer to get current result ")
+
+@bot.message_handler(func=lambda message: True)
+def listener(m):
+    if m.content_type == "text":
+        bot.send_message(m.chat.id, m.text)
+
+bot.polling()
---
+++ tutorial/config.py
@@ -0,0 +1,1 @@
+token = "127357990:AAF9UB9Bn1fUODcw-aoGNmEUkUXzcCu-US0"
