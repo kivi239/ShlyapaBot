e29e2d748341b3ee9f00436701765344bd7adfae
kivi239 <kivi239@gmail.com>
2016-03-19 23:00:01 +0300

    Remove config

--- game/config.py
+++
@@ -1,3 +0,0 @@
-token = "205861514:AAGLT5Elgp32Q1bgfGgGRBIB0i38KLMPY4o"
-syndict = ["../synonymdict/synmaster_utf.txt", "../synonymdict/synonyms_norm.txt"]
-corpuses = ["/Volumes/TRANSCEND/models/ruscorpora.model.bin"] #not in this folder(300 meg!)
