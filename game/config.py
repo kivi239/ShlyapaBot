a8e8be481a3c925f143419bd742b8c7e8b342021
nosov-pvl <nosov-pvl@yandex.ru>
2016-03-19 21:23:15 +0300

    Changed config

--- game/config.py
+++ game/config.py
@@ -1,3 +1,4 @@
 token = "205861514:AAGLT5Elgp32Q1bgfGgGRBIB0i38KLMPY4o"
 syndict = ["../synonymdict/synmaster_utf.txt", "../synonymdict/synonyms_norm.txt"]
 corpuses = ["/Volumes/TRANSCEND/models/ruscorpora.model.bin"] #not in this folder(300 meg!)
+bigrams = ["../Ngrams/bigrams_next.txt", "../Ngrams/bigrams_prev.txt"]
