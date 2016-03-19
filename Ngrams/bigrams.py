1943b754638307723e3dacaa097b680b6e574392
kivi239 <kivi239@gmail.com>
2016-03-19 21:18:52 +0300

    Merge

--- Ngrams/bigrams.py
+++ Ngrams/bigrams.py
@@ -2,6 +2,8 @@
 import pymorphy2
 import operator
 
+MAX_COUNT = 10
+
 def clear_word(word):
     letters = list("\n.,!?P()[]{}'\"")
     for letter in letters:
@@ -50,28 +52,28 @@ with open('ansi.txt') as f:
 #print(prev_words)
 #print(next_words)
 
-output = open('bigrams_next.txt', 'w')
+bigrams_next = open('bigrams_next.txt', 'w')
 for key in next_words.keys():
     sorted_next = sorted(next_words[key].items(), key=operator.itemgetter(1))
     if len(sorted_next) == 0:
         continue
 
-    output.write(key + ' ')
-    for i in range(min(10, len(sorted_next))):
-        output.write(sorted_next[i][0] + ' ')
-        output.write(str(sorted_next[i][1]))
-        output.write(' ')
-    output.write('\n')
+    bigrams_next.write(key + ' ')
+    for i in range(min(MAX_COUNT, len(sorted_next))):
+        bigrams_next.write(sorted_next[i][0] + ' ')
+        bigrams_next.write(str(sorted_next[i][1]))
+        bigrams_next.write(' ')
+    bigrams_next.write('\n')
 
-output2 = open('bigrams_prev.txt', 'w')
+bigrams_prev = open('bigrams_prev.txt', 'w')
 for key in prev_words.keys():
     sorted_prev = sorted(prev_words[key].items(), key=operator.itemgetter(1))
     if len(sorted_prev) == 0:
         continue
 
-    output2.write(key + ' ')
-    for i in range(min(10, len(sorted_prev))):
-        output2.write(sorted_prev[i][0] + ' ')
-        output2.write(str(sorted_prev[i][1]))
-        output2.write(' ')
-    output2.write('\n')
+    bigrams_prev.write(key + ' ')
+    for i in range(min(MAX_COUNT, len(sorted_prev))):
+        bigrams_prev.write(sorted_prev[i][0] + ' ')
+        bigrams_prev.write(str(sorted_prev[i][1]))
+        bigrams_prev.write(' ')
+    bigrams_prev.write('\n')
