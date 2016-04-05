671e01a8db3385b1ce40793c40234e0a54b0ec85
kivi239 <kivi239@gmail.com>
2016-04-05 15:02:10 +0300

    Extract nouns from ruscorpora

---
+++ Ngrams/bigrams/extract_nouns.py
@@ -0,0 +1,67 @@
+import pymorphy2
+import operator
+
+morph = pymorphy2.MorphAnalyzer()
+
+word_tags = dict()
+normal_forms = dict()
+
+next_words = dict()
+
+with open('../../BIG_files/2grams-3.txt', encoding='utf-8') as f:
+    for line in f:
+        line = line.rstrip("\n")
+        line = line.replace("\t\t", " ")
+        line = line.replace("\t", " ")
+
+        data = line.split(" ")
+        normal_form = ''
+        print(data)
+        if data[2] in normal_forms:
+            normal_form = normal_forms[data[2]]
+        else:
+            normal_form = morph.parse(data[2])[0].normal_form
+            normal_forms[data[2]] = normal_form
+            word_tags[normal_form] = morph.parse(normal_form)[0].tag
+
+        tags = []
+        if data[1] in word_tags:
+            tags = word_tags[data[1]]
+        else:
+            tags = morph.parse(data[1])[0].tag
+            word_tags[data[1]] = tags
+
+        if 'PREP' in tags:
+            continue
+
+        if 'NOUN' not in word_tags[normal_form]:
+            continue
+
+        freq = int(data[0])
+        if data[2] not in next_words:
+            next_words[data[2]] = dict()
+        if data[1] not in next_words[data[2]]:
+            next_words[data[2]][data[1]] = freq
+        else:
+            next_words[data[2]][data[1]] += freq
+
+#for word in next_words:
+#    for neighbor in next_words[word]:
+#        neighbor = morph.parse()
+
+
+g = open('ruscorp_bigrams_reordered.txt', 'w', encoding='utf-8')
+
+for word in next_words.keys():
+    sorted_next = sorted(next_words[word].items(), key=operator.itemgetter(1))
+    length = len(sorted_next)
+    if length == 0:
+        continue
+    g.write(word + ' ')
+    size = min(15, len(sorted_next))
+    for i in range(size):
+        g.write(sorted_next[length - size + i][0] + ' ')
+        g.write(str(sorted_next[length - size + i][1]))
+        g.write(' ')
+    g.write('\n')
+
